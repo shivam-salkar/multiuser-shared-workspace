6a561ebcad6ca3adb3a286f1659b14d1be6d18b3
Shivam Salkar <salkarshivam28@gmail.com>
2026-09-18 20:10:52 +0530

    init: basic backend installs and server setup

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { YSocketIO } from "y-socket.io/dist/server"; 
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    }
+});
+const ySocketIO = new YSocketIO(io);
+ySocketIO.initialize();
+
+const port = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        message: "ok",
+        status: "working",
+        timestamp: Date.now(),
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
